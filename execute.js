f9708be4360bde66709e42bd234ca4f87514a4d8
swiftyspiffy <swiftyspiffy@gmail.com>
2023-04-24 18:54:38 +0900

    cleanup ordering and fix order of operations bug in execute.js

--- execute.js
+++ execute.js
@@ -1,40 +1,37 @@
 var storage = chrome.storage.local;
 
-// global variables so that we can populate them initially from storage, and update
-// them from storage onChanged event, and then use them in onBeforeSendHeaders
-var clientId = "";
-var accessToken = "";
+getClientIdAndAccessTokenAndSetRules();
 
-storage.get(['client_id', 'access_token'], function(result) {
-    if (result != undefined && result.client_id != undefined && result.access_token != undefined) {
-        clientId = result.client_id;
-        accessToken = result.access_token;
-    }
-});
+function getClientIdAndAccessTokenAndSetRules() {
+    storage.get(['client_id', 'access_token'], function(result) {
+        if (result != undefined && result.client_id != undefined && result.access_token != undefined) {
+            const clientId = result.client_id;
+            const accessToken = result.access_token;
+    
+            // add rule to set headers after storage get call completes successfully
+            chrome.declarativeNetRequest.updateDynamicRules({
+                addRules: [{
+                    id: 1001, // A unique ID for your rule
+                    priority: 1, // The priority of your rule
+                    action: {
+                        type: "modifyHeaders",
+                        requestHeaders: [
+                            { "header": "Client-ID", "operation": "set", "value": clientId},
+                            { "header": "Authorization", "operation": "set", "value": "Bearer " + accessToken}
+                        ]
+                    },
+                    condition: { "urlFilter": "https://api.twitch.tv/helix/*", "resourceTypes": ["main_frame"] }
+                }],
+                removeRuleIds: [1001]
+            });
+            console.log("client id (" + clientId + ") and access token (" + accessToken + ") dynamic rule added!");
+        } else {
+            console.log("failed to retrieve client id and access token from storage");
+        }
+    });
+}
 
 storage.onChanged.addListener(function(changes, area) {
-    console.log(changes);
-    if ("client_id" in changes) {
-        clientId = changes.client_id.newValue;
-    }
-    if ("access_token" in changes) {
-        accessToken = changes.access_token.newValue;
-    }
-});
-
-chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [], // IDs of rules to remove
-    addRules: [{
-        id: 1, // A unique ID for your rule
-        priority: 1, // The priority of your rule
-        action: {
-            type: "modifyHeaders",
-            requestHeaders: [
-                { "header": "Client-ID", "operation": "set", "value": clientId},
-                { "header": "Authorization", "operation": "set", "value": "Bearer " + accessToken}
-            ]
-        },
-        condition: { "urlFilter": "https://api.twitch.tv/helix/*", "resourceTypes": ["main_frame"] }
-    }],
-    removeRuleIds: [1]
+    console.log("storage updated! re-setting rules");
+    getClientIdAndAccessTokenAndSetRules();
 });
